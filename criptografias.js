c588c2a95ff012dfc8a8accdd20b357f4b5717ab
Ana Carolina R Rocha <anacarolina.rrocha@gmail.com>
2022-09-11 16:30:03 -0300

    falta mt trem, mas vou sair agr

--- criptografias.js
+++ criptografias.js
@@ -19,7 +19,7 @@ class Criptografias {
         deCesar(){
 
             let mensagemCriptografada = '';
-            
+
             Object.entries(this.mensagem).forEach((indexLetra) => {
                 mensagemCriptografada += this.criptografiaAlfabetica(indexLetra,this.CRIPTO_CESAR);
             });
@@ -28,7 +28,49 @@ class Criptografias {
         }
 
         dePolimorfica(){ 
-            return;
+            let mensagemCriptografada = '';
+            let mensagem = '';
+            let chaveCriptografada = '';
+            let tamanhoChave = this.chave.length;
+            let cont = 0;
+            
+            Object.entries(this.mensagem).forEach((indexLetra,index) => {
+                
+                mensagem += indexLetra[1];
+
+                if( index === (tamanhoChave + cont - 1)){
+                    cont += tamanhoChave; 
+                    mensagem = mensagem.concat('.') 
+                }
+                
+            });
+            
+            let arrayMessagem = mensagem.split('.');
+            let testesoma = '';
+            Object.entries(arrayMessagem[0]).forEach((indexLetra, indexMensagem) => {
+                let caracteresValuesMensagem = getIndexOnCaracteres(indexLetra[1]);
+
+                Object.entries(this.chave).forEach((indexLetraChave, indexChave) => { 
+                    let caracteresValuesChave = getIndexOnCaracteres(indexLetraChave[1])
+
+                    if(indexMensagem = indexChave ){
+                        
+                        TESTE SOMA É ´P SER A CRIPTGRAFIA
+                        testesoma += caracteres[(getIndexOnCaracteres(caracteresValuesMensagem + caracteresValuesChave))]; 
+                        FALTA UM CALCULO DE PORCENTAGEM BEM AQUI Q N ME LEMBRO
+                        console.log(testesoma)
+                    }
+                    
+                });
+            
+                
+            });
+
+            Object.entries(this.chave).forEach((indexLetra) => {
+                chaveCriptografada += this.criptografiaAlfabetica(indexLetra,this.CRIPTO_POLIMORFICA);
+            });
+
+           /// return [mensagemCriptografada,chaveCriptografada];
         }
         
         dePlayFair(){
@@ -46,13 +88,17 @@ class Criptografias {
 
             if(tipoCriptografia === this.CRIPTO_CESAR){
                 return caracteres[(getIndexOnCaracteres(letra) + 3)]
-            }else if(tipoCriptografia === this.CRIPTO_POLIMORFICA){
-
+            }else if(tipoCriptografia === this.CRIPTO_POLIMORFICA){ //somar a letra da mensagem mais a letra da chave
+             
+                    return caracteres[(getIndexOnCaracteres(letra) + 3)]
+                
             }else if(tipoCriptografia === this.CRIPTO_PLAYFAIR){
 
             }
         }
 }
 
-let criptografias = new Criptografias("CAROL");
-console.log(criptografias.deCesar());
+let criptografiasDeCesar = new Criptografias("CAROL");
+    //console.log(criptografias.deCesar());
+let criptografias = new Criptografias("VAMOSATACARAMANHASEMFALTA", "GRITO");
+    console.log(criptografias.dePolimorfica());
